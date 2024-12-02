e69136b9f5edaedb4adf1505e4513e75a9b29e40
Xigui Chen <xigui.chen@citrix.com>
2024-12-02 21:10:25 +0800

    add support for read the curernt word under cursor

--- lua/nvim-read-aloud/init.lua
+++ lua/nvim-read-aloud/init.lua
@@ -18,7 +18,35 @@ function M.read_current_line(lang)
     local line = vim.api.nvim_get_current_line()
     local encoded_line = url_encode(line)
     local tts_url = "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=" ..
-    encoded_line .. "&tl=" .. lang
+        encoded_line .. "&tl=" .. lang
+    local temp_file = vim.fn.tempname() .. ".mp3"
+    Job:new({
+        command = 'curl',
+        args = { '-s', tts_url, '-o', temp_file },
+        on_exit = function(j, return_val)
+            if return_val == 0 then
+                Job:new({
+                    command = 'mpv',
+                    args = { temp_file },
+                    detached = true,
+                    on_exit = function()
+                        os.remove(temp_file)
+                    end,
+                }):start()
+            else
+                error("Failed to fetch TTS audio")
+            end
+        end,
+    }):start()
+end
+
+-- Function to read the current word
+function M.read_current_word(lang)
+    lang = lang and lang ~= "" and lang or "en" -- Default to English if no language is provided
+    local word = vim.fn.expand("<cword>")
+    local encoded_word = url_encode(word)
+    local tts_url = "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=" ..
+        encoded_word .. "&tl=" .. lang
     local temp_file = vim.fn.tempname() .. ".mp3"
     Job:new({
         command = 'curl',
@@ -49,7 +77,7 @@ function M.read_selected_text(range, lang)
     local text = table.concat(lines, " ")
     local encoded_text = url_encode(text)
     local tts_url = "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=" ..
-    encoded_text .. "&tl=" .. lang
+        encoded_text .. "&tl=" .. lang
     local temp_file = os.tmpname() .. ".mp3"
     Job:new({
         command = 'curl',
@@ -80,6 +108,10 @@ function M.setup()
     vim.api.nvim_create_user_command('ReadSelectedText', function(opts)
         M.read_selected_text({ opts.line1, opts.line2 }, opts.args)
     end, { range = true, nargs = '?' }) -- nargs = '?' allows zero or one argument
+
+    vim.api.nvim_create_user_command('ReadCurrentWord', function(opts)
+        M.read_current_word(opts.args)
+    end, { nargs = '?' })     -- nargs = '?' allows zero or one argument
 end
 
 return M
